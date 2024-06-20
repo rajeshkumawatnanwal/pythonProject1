17c992523a489a2b48b7a453a00563cabfe378be
rajeshkumawatnanwal <160112828+rajeshkumawatnanwal@users.noreply.github.com>
2024-06-20 12:07:32 +0530

    Update capstone project.py

--- capstone project.py
+++ capstone project.py
@@ -1,1 +1,18 @@
-this file is for demo only.
+def make_notes():
+    speak("speak that you want to make notes")
+    print("speak that you want to make notes")
+    note ,_ = listen()
+    if note:
+        filename = f"note_by_voice.txt"
+        with open(filename, 'w') as file:
+            file.write(note)
+        print(f"Notes saved as {filename}")
+        speak(f"Notes saved as {filename}")
+    else:
+        print("please speak your notes.")
+        speak("please speak your notes.")
+
+# add it in main function
+if "notes" in command:
+  make_notes()
+                        
